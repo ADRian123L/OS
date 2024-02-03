406feab954a2d2b6d716313d2310c503b7bccf66
ADRian123L <adrian163@usf.edu>
2024-02-03 00:04:07 -0500

    Made changes to main

--- RUSH/main.c
+++ RUSH/main.c
@@ -1,11 +1,20 @@
 #include <fcntl.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
+#define PROMPT strdup("rush> \0")
+
 int main(int argc, char *argv[]) {
-    char *Message = "It compiles\n";
-    write(STDOUT_FILENO, Message, strlen(Message));
+    char  *input = NULL;
+    size_t size  = 0;
+    while (true) {
+        fprintf(stdout, "%s", PROMPT);
+        getline(&input, &size, stdin);
+        write(STDIN_FILENO, input, strlen(input));
+        fflush(STDIN_FILENO);
+    }
     return 0;
 }
